84a8eb9f3ed1552f1b6fe3021534f1253b2e3147
Marcus Boay <marcusboay@hotmail.com>
2022-12-04 17:44:53 -0500

    refactor 3.2

--- src/3.rs
+++ src/3.rs
@@ -7,33 +7,39 @@ fn main() {
     get_life_support_rating(&data);
 }
 
+fn get_ones(data: &Vec<String>, i: usize) -> i32 {
+    let mut ones = 0;
+    for line in data {
+        let b = line.chars().into_iter().collect::<Vec<char>>()[i];
+        if b == '1' {
+            ones += 1;
+        } else {
+            ones -= 1;
+        }
+    }
+    ones
+}
+
 fn get_power_consumption(data: &Vec<String>) {
     let mut epsilon_rate = 0;
     let mut gamma_rate = 0;
     for i in 0..data[0].len() {
         epsilon_rate <<= 1;
         gamma_rate <<= 1;
-        let mut is_one_common = 0;
-        for line in data {
-            let b = line.chars().into_iter().collect::<Vec<char>>()[i];
-            if b == '1' {
-                is_one_common += 1;
-            } else {
-                is_one_common -= 1;
-            }
-        }
+        let ones = get_ones(data, i);
 
-        epsilon_rate |= (is_one_common > 0) as i32;
-        gamma_rate |= (is_one_common < 0) as i32;
+        epsilon_rate |= (ones > 0) as i32;
+        gamma_rate |= (ones < 0) as i32;
     }
     println!("Power consumption: {}", epsilon_rate * gamma_rate);
 }
 
 fn get_life_support_rating(data: &Vec<String>) {
+    #[derive(PartialEq)]
     enum CommonType {
-        MOST,
-        LEAST,
-    };
+        Most,
+        Least,
+    }
 
     fn get_common(data: &Vec<String>, common_type: CommonType) -> u32 {
         let mut lines = data.clone();
@@ -41,48 +47,35 @@ fn get_life_support_rating(data: &Vec<String>) {
         loop {
             let mut new_lines: Vec<String> = vec![];
             // Get common element first.
-            let mut is_one_common = 0;
-            for line in &lines {
-                let b = line.chars().into_iter().collect::<Vec<char>>()[i];
-                if b == '1' {
-                    is_one_common += 1;
-                } else {
-                    is_one_common -= 1;
-                }
-            }
+            let ones = get_ones(&lines, i);
 
             // Get lines with common bit.
-            let most_common = match common_type {
-                CommonType::MOST => {
-                    if is_one_common >= 0 {
-                        '1'
-                    } else {
-                        '0'
-                    }
+            let common_bit = if ones >= 0 {
+                if common_type == CommonType::Most {
+                    '1'
+                } else {
+                    '0'
                 }
-                CommonType::LEAST => {
-                    if is_one_common >= 0 {
-                        '0'
-                    } else {
-                        '1'
-                    }
-                }
+            } else if common_type == CommonType::Most {
+                '0'
+            } else {
+                '1'
             };
             for line in &lines {
                 let b = line.chars().into_iter().collect::<Vec<char>>()[i];
-                if b == most_common {
+                if b == common_bit {
                     new_lines.push(line.clone());
                 }
             }
 
             i = (i + 1) % lines[0].len();
-
             lines = new_lines;
             if lines.len() <= 1 {
                 break;
             }
         }
 
+        // Convert binary to decimal.
         let mut decimal_value = 0;
         for b in lines[0].chars() {
             decimal_value <<= 1;
@@ -91,8 +84,8 @@ fn get_life_support_rating(data: &Vec<String>) {
         decimal_value
     }
 
-    let oxygen_generator_rating = get_common(&data, CommonType::MOST);
-    let co2_scrubber_rating = get_common(&data, CommonType::LEAST);
+    let oxygen_generator_rating = get_common(data, CommonType::Most);
+    let co2_scrubber_rating = get_common(data, CommonType::Least);
 
     println!(
         "Life support rating: {}",
